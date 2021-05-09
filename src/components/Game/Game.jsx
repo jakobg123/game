f83f9c3f14af7b8edf411b405498bfbf461eda98
jakobg123 <jakob.gauffin45@gmail.com>
2021-05-09 18:59:39 +0200

    Rename Board to Game and clean up

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,28 +1,14 @@
 import {useState, useEffect} from 'react';
-import "./Board.scss";
+import "./Game.scss";
 
-const Board = () => {
+const Game = () => {
     const [snake, setSnake] = useState([]);
-    console.log("OUTPUT ÄR ~ file: Board.jsx ~ line 6 ~ Board ~ snake", snake)
     const [obstacles, setObstacles] = useState([]);
-    console.log("OUTPUT ÄR ~ file: Board.jsx ~ line 8 ~ Board ~ obstacles", obstacles)
     const [validMoves, setValidMoves] = useState([]);
 
-    const [allMoves, setAllMoves] = useState(0);
-    const [allObstacles, setAllObstacles] = useState(0);
-
-    const [restartGame, setRestartGame] = useState(false)
+    const [score, setScore] = useState(0);
+    const [startGame, setStartGame] = useState(false)
     
-    // const [boardDimension, setBoardDimension] = useState(
-    //     {
-    //         // rows: 2,
-    //         // cols: 2,
-    //         rows: 8,
-    //         cols: 8,
-    //         grid: [],
-    //     }
-    // )
-
     const BOARD_ROWS = 8;
     const BOARD_COLUMNS = 8;
     
@@ -37,71 +23,30 @@ const Board = () => {
         setValidMoves([]);
         setSnake([]);
         setObstacles([]);
-        setAllMoves(0);
-        setRestartGame(!restartGame);
+        setScore(0);
+        setStartGame(!startGame);
     }
-
-    // useEffect(() => {
-    //     if(!validMoves.length && !!obstacles.length){
-    //         alert("Game over");
-    //     }
-    // }, [validMoves])
 
     useEffect(() => {
             const snakeHead = getRandomPosition();
             setSnake([...snake, snakeHead]);
-    }, [restartGame])
-
-    // useEffect(() => {
-    //     // if(!boardDimension.grid.length){
-
-    //         const snakeHead = getRandomPosition();
-    //         setSnake([...snake, snakeHead]);
-    //         // setBoardDimension({...boardDimension, grid: gridData});
-    //     // }
-    // }, [restartGame])
+    }, [startGame])
 
     useEffect(() => {
         if(snake.length === 1){
             getValidMoves();
         }
 
-        // if(!!obstacles.length){
-            // let prevObstacles = [...obstacles];
-            // console.log("OUTPUT ÄR ~ file: Board.jsx ~ line 62 ~ useEffect ~ prevObstacles", prevObstacles)
-
-            // const updatedObstacles = updateObstacleCounterAndRemoveIfZero(prevObstacles)
-            // console.log("OUTPUT ÄR ~ file: Board.jsx ~ line 64 ~ useEffect ~ updatedObstacles", updatedObstacles)
-
-            // setObstacles(updatedObstacles);
-        // }
-
         if(snake.length > 1){
             getRandomObstacle();
         }
-
-        // getValidMoves();
     }, [snake])
 
     useEffect(() => {
         getValidMoves();
     }, [obstacles])
 
-    // useEffect(() => {
-
-    //     if(!!obstacles.length){
-    //         let prevObstacles = [...obstacles];
-
-    //         const updatedObstacles = updateObstacleCounterAndRemoveIfZero(prevObstacles)
-    //         console.log("OUTPUT ÄR ~ file: Board.jsx ~ line 64 ~ useEffect ~ updatedObstacles", updatedObstacles)
-
-    //         setObstacles(updatedObstacles);
-    //     }
-        
-    // }, [allMoves])
-
     const updateObstacleCounterAndRemoveIfZero = (prevObstacles) => {
-        console.log("uppdateringscountern körs");
         return prevObstacles.reduce((filtered, obstacle) => {
             const updatedCounter = obstacle.counter - 1;
 
@@ -122,20 +67,18 @@ const Board = () => {
                 moves = [{row: snake[0].row - 1, col: snake[0].col}]
             }
             if(checkMove("right")){
-                console.log("höger");
                 moves = [...moves, {row: snake[0].row, col: snake[0].col + 1}]
             }
             if(checkMove("down")){
-                console.log("neråt");
                 moves = [...moves, {row: snake[0].row + 1, col: snake[0].col}]
             }
             if(checkMove("left")){
-                console.log("vänster");
                 moves = [...moves, {row: snake[0].row, col: snake[0].col - 1}]
             }
         }
         setValidMoves(moves);
     }
+
     const checkMove = (direction) => {
         let move = {};
 
@@ -153,10 +96,6 @@ const Board = () => {
                 move = {row: snake[0].row, col: snake[0].col - 1};
                 break;
             }
-        //TODO: den här logiken är inte toppen, man vill inte att den ska falla utanför banan, så ta hänsyn till både row och column, just nu bara row
-        // if(move < 0 || move > boardDimension.row || snake.some((snakePart) => checkIfPartIsOnCell(snakePart, move)) || obstacles.some((obstacle) => checkIfPartIsOnCell(obstacle, move))){
-        //     return false;
-        // }
         
         if(move.row < 0 || move.row >= BOARD_ROWS || move.col < 0 || move.col >= BOARD_COLUMNS){
             return false;
@@ -183,45 +122,27 @@ const Board = () => {
     }
 
     const getRandomObstacle = () => {
-        // let randomObstacle = {};
         let randomObstacle = getRandomPosition();
-        console.log("OUTPUT ÄR ~ file: Board.jsx ~ line 154 ~ getRandomObstacle ~ randomObstacle", randomObstacle)
-        console.log("OUTPUT ÄR ~ file: Board.jsx ~ line 153 ~ getRandomObstacle ~ snake", snake)
-
+        
         if(snake.some((snakePart) => checkIfPartIsOnCell(snakePart, randomObstacle))){
-            console.log("FEEEEEEEEEEEEEEEEEEEELLLLLLLLLLLLL: ", randomObstacle);
             return getRandomObstacle();
         }
         
         if(obstacles.some((obstacle) => checkIfPartIsOnCell(obstacle, randomObstacle))){
-            console.log("161 FEEEEEEEEEEEEEEEEEEEELLLLLLLLLLLLL: ", randomObstacle);
             return getRandomObstacle();
         }
 
         randomObstacle.counter = 20;
         let updatedObstacles = updateObstacleCounterAndRemoveIfZero([...obstacles]);
-        console.log("NU SÄTTS EN OBSTACLE");
         return setObstacles([...updatedObstacles, randomObstacle]);
-        // return setObstacles([...obstacles, randomObstacle]);
     }
 
-    const getRandomPosition = () => {
-        console.log("den slumpar fram ett nytt tal");
-        return (
-            {
-                row: Math.floor(Math.random() * BOARD_ROWS),
-                col: Math.floor(Math.random() * BOARD_COLUMNS),
-            }
-
-        )
-    }
-
-    // const getRandomPosition = () => (
-    //     {
-    //         row: Math.floor(Math.random() * boardDimension.rows),
-    //         col: Math.floor(Math.random() * boardDimension.cols),
-    //     }
-    // )
+    const getRandomPosition = () => (
+        {
+            row: Math.floor(Math.random() * BOARD_ROWS),
+            col: Math.floor(Math.random() * BOARD_COLUMNS),
+        }
+    )
 
     const handleClick = (direction) => {
         let newDirection = {row: snake[0].row - direction.row, col: snake[0].col - direction.col};
@@ -229,19 +150,15 @@ const Board = () => {
         
         switch(JSON.stringify(newDirection)){
             case JSON.stringify({ row: 1, col: 0 }):
-                console.log("up");
                 change = "up";
                 break;
             case JSON.stringify({ row: -1, col: 0 }):
-                console.log("down");
                 change = "down";
                 break;
             case JSON.stringify({ row: 0, col: 1 }):
-                console.log("left");
                 change = "left";
                 break;
             case JSON.stringify({ row: 0, col: -1 }):
-                console.log("right");
                 change = "right";
                 break;
         }
@@ -277,53 +194,14 @@ const Board = () => {
     }
 
     const updateGame = (updatedSnake) => {
-        
         setSnake(updatedSnake);
-        // getRandomObstacle();
-        setAllMoves(allMoves + 1);
-        // setAllObstacles(allObstacles + 1);
+        setScore(score + 1);
     }
-
-    // const handleKeyUp = (e) => {
-    //     console.log(e.key);
-    //     console.log(e.keyCode);
-    //     let newDirection = {};
-
-    //     switch (e.keyCode) {
-    //         case 37:
-    //             // console.log("left");
-    //             newDirection = {row: snake[0].row, col: snake[0].col - 1};
-    //             break;
-    //         case 38:
-    //             // console.log("up");
-    //             newDirection = {row: snake[0].row - 1, col: snake[0].col};
-    //             break;
-    //         case 39:
-    //             // console.log("right");
-    //             newDirection = {row: snake[0].row, col: snake[0].col + 1};
-    //             break;
-    //         case 40:
-    //             // console.log("down");
-    //             newDirection = {row: snake[0].row + 1, col: snake[0].col};
-    //         break;
-    //     }
-
-    //     let updatedSnake = [...snake];
-    //     updatedSnake.unshift(newDirection);
-    //     if(updatedSnake.length > 7){
-    //         updatedSnake.pop();
-    //     }
-    //     getRandomObstacle();
-    //     setSnake(updatedSnake);
-    //     setAllMoves(allMoves + 1);
-    //     setAllObstacles(allObstacles + 1);
-    // }
 
     const checkIfPartIsOnCell = (part, cell) => part.row === cell.row && part.col === cell.col;
 
     const getSnakeStyle = (index) => {
         const scaleValue = 1 - (index/7);
-        // const scaleValue = 1 - (index/snake.length);
         return {transform: `scale(${scaleValue.toString()})`};
     }
 
@@ -335,10 +213,8 @@ const Board = () => {
         return counter;
     }
     
-
     const gridJsx = gridData.map((cell, index) => (
         <div className={`Board__GridCell${getWhiteCellColor(cell.row, index)}`} key={cell.row.toString()+ cell.col.toString()}>
-            {/* {cell.row + "/" + cell.col} */}
 
                 {snake.some((snakePart) => checkIfPartIsOnCell(snakePart, cell)) && (
                     <div 
@@ -364,8 +240,7 @@ const Board = () => {
 
     return (
         <>
-            <h1>Moves: {allMoves}</h1>
-            <h1>Obstacles: {allObstacles}</h1>
+            <h1>Score: {score}</h1>
             <div className="Board" tabIndex="0">            
                 {gridJsx}
                 {(!validMoves.length && !!obstacles.length) && (
@@ -385,20 +260,20 @@ const Obstacle = ({counter}) => {
     let destroyClass = "";
 
     if(counter === 3){
-        destroyClass = " Board__Destroy--Three";
+        destroyClass = " Board__Destroy Board__Destroy--Three";
     }
     if(counter === 2){
-        destroyClass = " Board__Destroy--Two";
+        destroyClass = " Board__Destroy Board__Destroy--Two";
     }
     if(counter === 1){
-        destroyClass = " Board__Destroy--One";
+        destroyClass = " Board__Destroy Board__Destroy--One";
     }
 
     return (
         <div 
         className={`Board__Obstacle${!!destroyClass ? destroyClass : ""}`} 
-        >{counter}</div>
+        ></div>
     )
 };
 
-export default Board
+export default Game
